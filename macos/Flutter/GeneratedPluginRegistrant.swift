7c7e0d8645778facce3b5ff60c42259d4e3aec97
Mo. Saleh Gholami <iSalehGholami@gmail.com>
2025-02-25 18:17:10 +0330

    Testing a rebase of project

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import desktop_window
-import flutter_local_notifications
-import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
